8117ae48e147ad53d1e9ce354b75709bc615ce46
prithviramanathan <prithvi.ramanathan@gmail.com>
2019-09-14 14:36:29 -0500

    changed units

--- API.py
+++ API.py
@@ -13,6 +13,7 @@ class Unit:
         self.attack = unit_json["attack"]
         self.isAlive = unit_json["isAlive"]
         self.id = unit_json["id"]
+        self.player_id = unit_json["playerId"]
         self.pos = Position(unit_json["pos"])
 
 
@@ -43,42 +44,35 @@ class Game:
     def update_game(self, game_json):
         self.game = game_json
 
-    def convert_json_to_units(self, units_json):
+    def get_units_for_team(self, player_id):
         units = []
-        for unit_json in units_json:
-            units.append(Unit(unit_json))
+        for row in self.game["map"]["tiles"]:
+            for tile in row:
+                if tile.unit and tile.unit.player_id == player_id:
+                    units.append(tile.unit)
         return units
 
     # Returns the player"s units
     def get_my_units(self):
-        if self.player_id == 1:
-            return self.convert_json_to_units(self.game["p1Units"])
-        else:
-            return self.convert_json_to_units(self.game["p2Units"])
+        self.get_units_for_team(self.player_id)
 
     # Returns the opponents units
     def get_enemy_units(self):
         if self.player_id == 1:
-            return self.convert_json_to_units(self.game["p2Units"])
+            return self.get_units_for_team(2)
         else:
-            return self.convert_json_to_units(self.game["p1Units"])
+            return self.get_units_for_team(1)
 
     # Return the tile object at a give x and y. Takes a position tuple (x,y)
     def get_tile(self, position):
-        tile_json =  self.game["map"]["tiles"][position[1]][position[0]]
+        tile_json =  self.game["map"]["tiles"][position[0]][position[1]]
         return Tile(tile_json)
 
     # Returns the unit at the given position, if there is one. Takes a position
     # tuple (x,y)  v
     def get_unit_at(self, position):
         tile = self.get_tile((position[0], position[1]))
-        for unit in self.get_my_units():
-            if unit.id == tile.unit:
-                return unit
-        for unit in self.get_enemy_units():
-            if unit.id == tile.unit:
-                return unit
-        return None
+        return tile.unit
     # Get the shortest valid path from start to end position while avoiding tiles in tiles_to_avoid
     # Start and end position are tuples (x,y). tiles_to_avoid is a list of such tuples
     def path_to(self, start_position, end_position, tiles_to_avoid=[]):
@@ -99,12 +93,12 @@ class Game:
                 left_directions.append("LEFT")
                 q.put((left, left_directions))
             right = (position[0] + 1, position[1])
-            if not ((right[0] >= len(self.game["map"]["tiles"][0])) or (right in tiles_to_avoid) or (self.get_tile(right).type != "BLANK")):
+            if not ((right[0] >= len(self.game["map"]["tiles"])) or (right in tiles_to_avoid) or (self.get_tile(right).type != "BLANK")):
                 right_directions = copy.copy(directions)
                 right_directions.append("RIGHT")
                 q.put((right, right_directions))
             down = (position[0], position[1] + 1)
-            if not ((down[1] >= len(self.game["map"]["tiles"])) or (down in tiles_to_avoid) or (self.get_tile(down).type != "BLANK")):
+            if not ((down[1] >= len(self.game["map"]["tiles"][0])) or (down in tiles_to_avoid) or (self.get_tile(down).type != "BLANK")):
                 down_directions = copy.copy(directions)
                 down_directions.append("DOWN")
                 q.put((down, down_directions))
@@ -158,7 +152,7 @@ class Game:
                     y_pos = position[1]
                 x_coordinate = x_pos + col - 3
                 y_coordinate = y_pos + row - 3
-                if x_coordinate >= 0 and x_coordinate < len(self.game["map"]["tiles"][0]) and y_coordinate >= 0 and y_coordinate < len(self.game["map"]["tiles"]):
+                if x_coordinate >= 0 and x_coordinate < len(self.game["map"]["tiles"]) and y_coordinate >= 0 and y_coordinate < len(self.game["map"]["tiles"][0]):
                     attacks.append((attack, Position({"x": x_coordinate, "y": y_coordinate})))
         return attacks
 
