8b6bc6857de30001a294bddeed2aca814969ef4e
Prithvi <prithvi.ramanathan@gmail.com>
2019-09-18 22:05:04 -0500

    fixed unit setup

--- Strategy.py
+++ Strategy.py
@@ -3,11 +3,14 @@ from API import Game
 
 class Strategy(Game):
     def get_setup(self):
-        return [{
-            "attackPattern": [[0] * 7 for j in range(7)],
-            "health": 5,
-            "speed": 4
-        } for i in range(3)]
+        units = []
+        for i in range(3):
+            unit = {"health": 5, "speed": 4}
+            unit["attackPattern"] = [[0] * 7 for j in range(7)]
+            unit["unitId"] = i + 1
+            unit["terrainPattern"] = [[False]*7 for j in range(7)]
+            units.append(unit)
+        return units
 
     def do_turn(self):
         decision = {}
